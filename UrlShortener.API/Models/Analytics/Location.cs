feb6d1d23704935ccb144182ae92d32f62b43412
reactash <ashishnegu9145@gmail.com>
2025-06-15 14:06:19 +0530

    changes in location class

--- UrlShortener.API/Models/Analytics/Location.cs
+++ UrlShortener.API/Models/Analytics/Location.cs
@@ -1,14 +1,31 @@
+using System.Text.Json.Serialization;
+
 namespace UrlShortener.API.Models.Analytics
 {
     public class Location
     {
+        [JsonPropertyName("country")]
         public string Country { get; set; }
+
+        [JsonPropertyName("city")]
         public string City { get; set; }
+
+        [JsonPropertyName("region")]
         public string Region { get; set; }
+
+        [JsonPropertyName("timezone")]
         public string Timezone { get; set; }
+
+        [JsonPropertyName("isp")]
         public string Isp { get; set; }
+
+        [JsonPropertyName("regionName")]
         public string RegionName { get; set; }
+
+        [JsonPropertyName("zip")]
         public string Zip { get; set; }
+
+        [JsonPropertyName("countryCode")]
         public string CountryCode { get; set; }
     }
 }
